2b4a1f0235daa5632a3a5bf8cc7c118adaf9fbbe
NguyenDaoTrongTin <tin.nguyen2402061@std.ttu.edu.vn>
2025-09-04 09:53:22 +0700

    Build a cordinate

--- 1D-Motion-car.py
+++ 1D-Motion-car.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image, ImageTk, ImageFont, ImageDraw, ImageGrab
 
@@ -15,9 +16,27 @@ class interface:
         self.canvas.pack()
 
     def motion(self):
-        self.img = Image.fromarray(cv2.resize(cv2.imread("car.png"), (100,100)))
+        self.img = Image.fromarray(cv2.resize(cv2.imread("car_rgba.png", cv2.IMREAD_UNCHANGED), (100,100)))
+        self.img_flip = Image.fromarray(cv2.flip(np.array(cv2.resize(cv2.imread("car_rgba.png", cv2.IMREAD_UNCHANGED), (100,100))), 1))
         self.img_tk = ImageTk.PhotoImage(self.img)
         self.img_id = self.canvas.create_image(self.screen_width/2, self.screen_height/2,image= self.img_tk)
+
+        self.cordinate_x = self.canvas.create_line(30, 10, 10, 30, 50, 30, 30, 10, 30, 310, 430, 310, 410, 290, 410, 330, 430, 310)
+        self.canvas.create_polygon(30, 10, 10, 30, 50, 30, fill= "black", outline= "black")
+        self.canvas.create_polygon(430, 310, 410, 290, 410, 330, fill= "black", outline= "black")
+        self.canvas.create_text(20, 320, fill= "black", font= ("Times New Roman", 15), text= "O")
+
+        y0 = 305
+        y1 = 315
+        for i in range(80, 401, 50):
+            self.canvas.create_line(i, y0, i, y1)
+            self.canvas.create_text(i, 330, fill= "black", font= ("Times New Roman", 10), text= f"{i-30}")
+
+        x0 = 25
+        x1 = 35
+        for y in range(260, 11, -50):
+            self.canvas.create_line(x0, y, x1, y)
+            self.canvas.create_text(15, y, fill= "black", font= ("Times New Roman", 10), text= f"{310-y}")
 
 if __name__ == "__main__":
     root = tk.Tk()
